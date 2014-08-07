990ada34f7be237d30a39c8e2259e5758e8e500f
Luis Henrique Borges <lshsousa@gmail.com>
2014-08-06 23:51:39 -0300

    Parse csv

--- parse_csv.py
+++ parse_csv.py
@@ -1,6 +1,30 @@
 import csv
+import json
 
-with open('v.csv', 'rb') as f:
+folder = 'data/candidatos/csv/MG/'
+filename = folder + 'governador.csv'
+
+with open(filename, 'rb') as f:
     r = csv.reader(f, delimiter=';', quotechar='|')
+    data = []
+    indexes = None
+
+    i = 0
     for row in r:
-        print ', '.join(row)
+        if i == 0:
+            indexes = row
+        else:
+            if indexes:
+                j = 0
+                d = {}
+                for index in indexes:
+                    if (j == len(indexes) - 1)  or j == 4:
+                        d[indexes[j]] = row[j]
+                    else:
+                        d[indexes[j]] = row[j].decode('utf-8').lower().title()
+                    j += 1
+                
+                data.append(d)
+        i += 1
+
+    print json.dumps(data)
